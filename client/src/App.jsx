8759c59541e8618fdb3eb31a578952f9692da246
Marcel Prisacari <marcel.prisacari.com>
2023-04-01 13:22:52 -0400

    updating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import "./App.css";
-import Nav from "./components/Nav/Nav";
 import News from "./components/News/News";
 import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
@@ -8,31 +6,9 @@ import { Route, Routes } from "react-router-dom";
 function App() {
     return (
         <div className="App">
-            <Nav>
-              
-            </Nav>
+            <News></News>
         </div>
     );
-=======
-import './App.css';
-import Footer from './components/footer/Footer';
-import Testimonials from './pages/Testimonials';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <div className="App">
-  
-        <Router>
-            <Routes>
-                <Route path='/testimonials' element={<Testimonials/>} />
-            </Routes>
-        </Router>  
-
-      <Footer /> 
-    </div>
-  );
->>>>>>> 1317edaba24352776fa91c0cede2b47e635a50ad
 }
 
 export default App;
--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Newsletter } from "reactstrap";
 
 function News() {
-    return(
-        
-    )
+    return()
 }
 
 export default News;
