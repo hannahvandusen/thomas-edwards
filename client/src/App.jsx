dde583b0305aa662e82348738f39800f82d96851
Ethan-M-M <moskowitz.e.m@gmail.com>
2023-04-01 13:06:58 -0400

    Routes to Testimonials Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Testimonials from './pages/Testimonials';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Footer />
+  
+        <Router>
+            <Routes>
+                <Route path='/testimonials' element={<Testimonials/>} />
+            </Routes>
+        </Router>  
+
     </div>
   );
 }
