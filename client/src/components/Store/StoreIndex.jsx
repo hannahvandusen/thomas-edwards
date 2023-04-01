6e64c16edec3111380af7282d575100c6ea17595
Hannah Van Dusen <hannahvandusen@Hannahs-MacBook-Pro.local>
2023-04-01 14:18:32 -0400

    update to with store and navbar

---
+++ client/src/components/Store/StoreIndex.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button, CardImg, CardGroup } from 'reactstrap'
+
+
+function StoreIndex() {
+    return (
+        <>
+        <h1>Inner Famous Shop</h1>
+        <CardGroup>
+        <Card>
+        <CardImg
+        alt="Card image cap"
+        src="https://picsum.photos/318/180"
+        top
+        width="100%"
+        />
+        <CardBody>
+        <CardTitle tag="h5">
+            Card title
+        </CardTitle>
+        <CardSubtitle
+            className="mb-2 text-muted"
+            tag="h6"
+        >
+            Card subtitle
+        </CardSubtitle>
+        <CardText>
+            This card has supporting text below as a natural lead-in to additional content.
+        </CardText>
+        <Button>
+            Add to Cart
+        </Button>
+        <Button>
+            View More
+        </Button>
+        </CardBody>
+        </Card>
+        <Card>
+        <CardImg
+        alt="Card image cap"
+        src="https://picsum.photos/318/180"
+        top
+        width="100%"
+        />
+        <CardBody>
+        <CardTitle tag="h5">
+            Card title
+        </CardTitle>
+        <CardSubtitle
+            className="mb-2 text-muted"
+            tag="h6"
+        >
+            Card subtitle
+        </CardSubtitle>
+        <CardText>
+            This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+        </CardText>
+        <Button>
+            Add to Cart
+        </Button>
+        <Button>
+            View More
+        </Button>
+        </CardBody>
+        </Card>
+        </CardGroup>
+        </>
+    )
+}
+
+export default StoreIndex; 
