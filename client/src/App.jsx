62e2bb0d222b43f4c31c475dbdd134c08fc2fdad
Marcel Prisacari <marcel.prisacari.com>
2023-04-01 13:27:14 -0400

    no changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,45 @@
-import "./App.css";
-import News from "./components/News/News";
-import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText,
+    Collapse,
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-function App() {
+function Navigation(args) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
-        <div className="App">
-            <News></News>
-        </div>
+        // <div className='navbar'>
+        //     <a href="/">Home</a>
+        //     <a href="/">About Thomas</a>
+        //     <a href="/">Testimonials</a>
+        //     <a href="/">Newsletter</a>
+        //     <a href="/">Shop</a>
+        // </div>
+        <>
+            <Nav fill pills>
+                <NavbarBrand>Inner Famous</NavbarBrand>
+                <NavItem>
+                    <NavLink href="/">Home</NavLink>
+                </NavItem>
+            </Nav>
+
+            {/* </Navbar> */}
+        </>
     );
 }
 
-export default App;
+export default Navigation;
