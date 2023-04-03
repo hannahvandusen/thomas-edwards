fd8306e102bef21aaa210147a680c591e9714761
micah cole <micahjaxoncole@gmail.com>
2023-04-03 19:44:29 -0400

    more troubleshooting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Footer from './components/footer/Footer';
+import Testimonials from './pages/Testimonials';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StoreIndex from './components/Store/StoreIndex';
+import AboutMe from './components/About/AboutMe'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" >
+        <Navigation /> 
+        {/* <img src={logo} alt="logo" />  */}
+        <Router>
+            <Routes>
+                <Route path='/testimonials' element={<Testimonials/>} />
+                <Route path='/store' element={<StoreIndex/>} /> 
+                <Route path='/about' element={<AboutMe/>}/> 
+            </Routes>
+        </Router>  
+
+      <Footer /> 
     </div>
   );
 }
