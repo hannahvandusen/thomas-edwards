e3891402563946db52bbf364fcc9eb3f064bb7a6
Hannah Van Dusen <hannahvandusen@Hannahs-MacBook-Pro.local>
2023-04-03 18:30:01 -0400

    develop page update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Footer from './components/footer/Footer';
+import Testimonials from './pages/Testimonials';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StoreIndex from './components/Store/StoreIndex';
+import logo from './images/photo1.jpg'
+import AboutMe from './components/About/AboutMe'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" >
+        <Navigation /> 
+        {/* <img src={logo} alt="logo" />  */}
+        <Router>
+            <Routes>
+                <Route path='/testimonials' element={<Testimonials/>} />
+                <Route path='/store' element={<StoreIndex/>} /> 
+                <Route path='/about' element={<AboutMe/>}/> 
+            </Routes>
+        </Router>  
+
+      <Footer /> 
     </div>
   );
 }
