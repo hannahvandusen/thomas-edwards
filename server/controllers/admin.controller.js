b4015ce827f8a66d43d61d3f9790bddad2ed2d95
Hannah Van Dusen <hannahvandusen@Hannahs-MacBook-Pro.local>
2023-04-05 19:29:37 -0400

    update

---
+++ server/controllers/admin.controller.js
@@ -0,0 +1,68 @@
+const router = require("express").Router();
+const { Admin } = require('../models'); 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/signup", async (req, res) => {
+
+    try {
+        const admin = new Admin({
+            firstName: req.body.first,
+            lastName: req.body.last,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password,)
+        });
+
+        const newAdmin = await admin.save();
+        const token = jwt.sign({ id: newAdmin._id}, process.env.JWT, {
+            expiresIn: "1 year"
+        });
+
+        newAdmin ? res.status(200).json({
+            newAdmin,
+            message: "Success! Admin account created",
+            token
+        }) : 
+        res.status(404).json({
+            message: 'incomplete'
+        });
+    } catch (err) {
+        res.status(500).json({
+            Error: err.message
+        })
+    }
+}); 
+
+router.post("/login", async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        const admin = await Admin.findOne({ email: email });
+
+        if(!admin) throw new Error("Email or password does not match our records");
+
+        const passwordCheck = await bcrypt.compare(password, admin.password);
+        if (!passwordCheck) throw new Error("Email or password does not match our records");
+
+        const token = jwt.sign({ id: admin._id}, process.env.JWT, {
+            expiresIn: "1 year"
+        });
+
+        email ?
+        res.status(200).json({
+            admin,
+            token
+        }) :
+        res.status(404).json({
+            message: "Something went wrong"
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            Error: err.message
+        })
+    }
+})
+
+module.exports = router;
