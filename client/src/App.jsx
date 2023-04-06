65c135c643863403683702b87072a3c092901882
lukemitrani <lukemitrani@gmail.com>
2023-04-05 20:40:03 -0400

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Footer from './components/footer/Footer';
-import Testimonials from './pages/Testimonials';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import StoreIndex from './components/Store/StoreIndex';
-import AboutMe from './components/About/AboutMe'
+import "./App.css"
+import Navigation from "./components/Navigation/Navigation"
+import Footer from "./components/footer/Footer"
+import Testimonials from "./pages/Testimonials"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import StoreIndex from "./components/Store/StoreIndex"
+import AboutMe from "./components/About/AboutMe"
 
 function App() {
   return (
-    <div className="App" >
-        <Navigation /> 
-        <Router>
-            <Routes>
-                <Route path='/testimonials' element={<Testimonials/>} />
-                <Route path='/store' element={<StoreIndex/>} /> 
-                <Route path='/about' element={<AboutMe/>}/> 
-                <Route path='/newsletter'/> 
-            </Routes>
-        </Router>  
+    <div className="App">
+      <Navigation />
+      <Router>
+        <Routes>
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/store" element={<StoreIndex />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/newsletter" />
+        </Routes>
+      </Router>
 
-      <Footer /> 
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
