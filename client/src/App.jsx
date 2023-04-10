2c781ba846b18c7e0b20fde1b7bb612035cfc9ad
Marcel Prisacari <marcel.prisacari.com>
2023-04-10 18:07:38 -0400

    changed style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,38 @@
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Home from './pages/home/Home';
-import Footer from './components/footer/Footer';
-import Testimonials from './pages/Testimonials';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import StoreIndex from './components/Store/StoreIndex';
-import AboutMe from './components/About/AboutMe'
-import Admin from './components/admin/Admin'
-import Intake from './pages/Intake'
-import Logout from './components/admin/logout/Logout';
-import Account from './components/admin/account/Account';
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import Testimonials from "./pages/Testimonials";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StoreIndex from "./components/Store/StoreIndex";
+import AboutMe from "./components/About/AboutMe";
+import Admin from "./components/admin/Admin";
+import Intake from "./pages/Intake";
+import Logout from "./components/admin/logout/Logout";
+import Account from "./components/admin/account/Account";
+import Newsletter from "./pages/Newsletter";
 
 function App() {
+    return (
+        <div className="App">
+            <Navigation />
+            <Router>
+                <Routes>
+                    <Route path="/testimonials" element={<Testimonials />} />
+                    <Route path="/store" element={<StoreIndex />} />
+                    <Route path="/about" element={<AboutMe />} />
+                    <Route path="/newsletter" element={<Newsletter />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/intake" element={<Intake />} />
+                    <Route path="/admin/account" element={<Account />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
 
-  return (
-    <div className="App">
-      <Navigation />
-      <Router>
-        <Routes>
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/store" element={<StoreIndex />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/newsletter" />
-          <Route path="/admin" element={<Admin />}/> 
-          <Route path="/intake" element={<Intake />} />
-          <Route path='/admin/account' element={<Account />}/> 
-          <Route path='/' element={<Home />} /> 
-        </Routes>
-      </Router>
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    );
 }
 
-export default App
-
+export default App;
--- client/src/components/newsletter/RecentNews.jsx
+++ client/src/components/newsletter/RecentNews.jsx
@@ -1,44 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./RecentNews.css";
 
-const RecentNews = () => {
-    const [newsletterIndex, setNewsletterIndex] = useState(0);
-    const newsletters = [
-        {
-            title: "Newsletter 1",
-            content: "Here is some content for newsletter 1",
-            date: "March 2023",
-        },
-        {
-            title: "Newsletter 2",
-            content: "Here is some content for newsletter 2",
-            date: "February 2023",
-        },
-        {
-            title: "Newsletter 3",
-            content: "Here is some content for newsletter 3",
-            date: "January 2023",
-        },
-    ];
+function RecentNews() {
+    const [newsletters, setNewsletters] = useState([]);
 
-    const handlePrevious = () => {
-        setNewsletterIndex(
-            (newsletterIndex + newsletters.length - 1) % newsletters.length
-        );
-    };
-
-    const handleNext = () => {
-        setNewsletterIndex((newsletterIndex + 1) % newsletters.length);
-    };
+    useEffect(() => {
+        axios
+            .get("/api/newsletters")
+            .then((response) => {
+                setNewsletters(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     return (
-        <div>
-            <h2>{newsletters[newsletterIndex].title}</h2>
-            <p>{newsletters[newsletterIndex].content}</p>
-            <p>{newsletters[newsletterIndex].date}</p>
-            <button onClick={handlePrevious}>Previous</button>
-            <button onClick={handleNext}>Next</button>
+        <div className="recent-news">
+            <h3 className="section-title">Recent Newsletters</h3>
+            <div className="row">
+                {newsletters.slice(0, 3).map((newsletter) => (
+                    <div className="col-md-4 mb-4" key={newsletter.id}>
+                        <div className="card newsletter-card">
+                            <div className="card-img-top">
+                                <img
+                                    src={newsletter.image}
+                                    alt={newsletter.title}
+                                    className="img-fluid"
+                                />
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    {newsletter.title}
+                                </h5>
+                                <p className="card-text">{newsletter.date}</p>
+                                <a
+                                    href={newsletter.link}
+                                    className="btn btn-primary newsletter-btn"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
-};
+}
 
 export default RecentNews;
