0ed51db8454201cdc015c370e139c28db94077f4
Marcel Prisacari <marcel.prisacari.com>
2023-04-12 19:01:39 -0400

    fixed typos

--- server/index.js
+++ server/index.js
@@ -2,32 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const cors = require('cors'); 
+const cors = require("cors");
 const mongoose = require("mongoose");
 const DBURL = process.env.DBURL;
 
 //* controllers
-const adminController = require('./controllers/admin.controller');
-<<<<<<< HEAD
-const testimonialsController = require('./controllers/testimonials.controller');
-=======
-const intakeController = require('./controllers/intake.controller');
->>>>>>> 4f46348b818fdd91d88bd6465ec3a94f19ff0668
+const adminController = require("./controllers/admin.controller");
+const testimonialsController = require("./controllers/testimonials.controller");
 
 mongoose.connect(`${DBURL}`);
 const db = mongoose.connection;
 mongoose.set("strictQuery", true);
-db.once("open", () => console.log(`Connected: ${DBURL}`)); 
+db.once("open", () => console.log(`Connected: ${DBURL}`));
 app.use(express.json());
 
 app.use(cors());
 
 //* pages
-app.use('/admin', adminController); 
-<<<<<<< HEAD
-app.use('./testimonials', testimonialsController);
-=======
-app.use('/intake', intakeController); 
->>>>>>> 4f46348b818fdd91d88bd6465ec3a94f19ff0668
+app.use("/admin", adminController);
+app.use("./testimonials", testimonialsController);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
