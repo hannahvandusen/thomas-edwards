f24375024fb294b1bc8e49f11334c6c19b0f305d
Ethan-M-M <moskowitz.e.m@gmail.com>
2023-04-22 13:36:14 -0400

    Footer social media links pop up in new tabs.

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
     return(
        <footer className="footer" style={{color: "#16181e"}}>innerfamous.com &copy; {currentYear}
 
-        <p> <a href="https://twitter.com/innerfamousone" className="twitter-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faTwitter} size="1x"/></a>
-        <a href="https://www.linkedin.com/in/thomas-edwards-0739598/" className="linkedin-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faLinkedinIn} size="1x"/></a>
-        <a href="https://www.instagram.com/innerfamous/" className="instagram-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faInstagram} size="1x"/></a>
-        <a href="https://www.facebook.com/profile.php?id=100088780985542" className="facebook-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faFacebook} size="1x" /></a> </p>
+        <p> <a href="https://twitter.com/innerfamousone" target="_blank" className="twitter-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faTwitter} size="1x"/></a>
+        <a href="https://www.linkedin.com/in/thomas-edwards-0739598/" target="_blank" className="linkedin-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faLinkedinIn} size="1x"/></a>
+        <a href="https://www.instagram.com/innerfamous/" target="_blank" className="instagram-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faInstagram} size="1x"/></a>
+        <a href="https://www.facebook.com/profile.php?id=100088780985542" target="_blank" className="facebook-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faFacebook} size="1x" /></a> </p>
         <a href="/admin/" style={{fontSize: "small", color: "#16181e"}} className="link">Admin</a>
         </footer>
     )
