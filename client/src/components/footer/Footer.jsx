63143efa119cdc03f19cd169986a1420ed1cc251
Marcel Prisacari <marcel.prisacari.com>
2023-04-26 19:44:27 -0400

    accepted changes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -7,56 +7,46 @@ import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
     const currentYear = new Date().getFullYear();
 
-<<<<<<< HEAD
     return (
         <footer className="footer" style={{ color: "#16181e" }}>
             innerfamous.com &copy; {currentYear}
             <p>
                 <a
                     href="https://twitter.com/innerfamousone"
-                    className="twitter-social"
-                    style={{ color: "#16181e" }}
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="twitter-social"
+                    style={{ color: "#16181e" }}
                 >
                     <FontAwesomeIcon icon={faTwitter} size="1x" />
                 </a>
                 <a
                     href="https://www.linkedin.com/in/thomas-edwards-0739598/"
-                    className="linkedin-social"
-                    style={{ color: "#16181e" }}
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="linkedin-social"
+                    style={{ color: "#16181e" }}
                 >
                     <FontAwesomeIcon icon={faLinkedinIn} size="1x" />
                 </a>
                 <a
                     href="https://www.instagram.com/innerfamous/"
                     className="instagram-social"
-                    style={{ color: "#16181e" }}
                     target="_blank"
                     rel="noopener noreferrer"
+                    style={{ color: "#16181e" }}
                 >
                     <FontAwesomeIcon icon={faInstagram} size="1x" />
                 </a>
                 <a
                     href="https://www.facebook.com/profile.php?id=100088780985542"
-                    className="facebook-social"
-                    style={{ color: "#16181e" }}
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="facebook-social"
+                    style={{ color: "#16181e" }}
                 >
                     <FontAwesomeIcon icon={faFacebook} size="1x" />
                 </a>
-=======
-    return(
-        <footer className="footer" style={{color: "#16181e"}}>innerfamous.com &copy; {currentYear}
-            <p> 
-                <a href="https://twitter.com/innerfamousone" target="_blank" rel="noopener noreferrer" className="twitter-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faTwitter} size="1x"/></a>
-                <a href="https://www.linkedin.com/in/thomas-edwards-0739598/" target="_blank" rel="noopener noreferrer" className="linkedin-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faLinkedinIn} size="1x"/></a>
-                <a href="https://www.instagram.com/innerfamous/" className="instagram-social" target="_blank" rel="noopener noreferrer" style={{color: "#16181e"}}><FontAwesomeIcon icon={faInstagram} size="1x"/></a>
-                <a href="https://www.facebook.com/profile.php?id=100088780985542" target="_blank" rel="noopener noreferrer" className="facebook-social" style={{color: "#16181e"}}><FontAwesomeIcon icon={faFacebook} size="1x" /></a> 
->>>>>>> 0db1084b4080b30e500141202c18258dd3b7acf2
             </p>
             <a
                 href="/admin"
