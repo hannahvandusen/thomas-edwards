a182725fc47d0cbc3eeda4707431eccd4a58d431
lukemitrani <lukemitrani@gmail.com>
2023-04-29 19:08:59 -0400

    updates

--- client/src/components/carousel/Carousel.jsx
+++ client/src/components/carousel/Carousel.jsx
@@ -1,51 +1,53 @@
-import React, { useState } from 'react';
+import "../../App.css"
+
+import React, { useState } from "react"
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
   CarouselCaption,
-} from 'reactstrap';
+} from "reactstrap"
 
 const items = [
   {
     id: 1,
-    altText: 'Slide 1',
-    caption: 'Slide 1',
+    altText: "Slide 1",
+    caption: "Slide 1",
   },
   {
     id: 2,
-    altText: 'Slide 2',
-    caption: 'Slide 2',
+    altText: "Slide 2",
+    caption: "Slide 2",
   },
   {
     id: 3,
-    altText: 'Slide 3',
-    caption: 'Slide 3',
+    altText: "Slide 3",
+    caption: "Slide 3",
   },
-];
+]
 
 function TestimonialCarousel(props) {
   console.log(props.testimonials)
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [animating, setAnimating] = useState(false)
 
   const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  };
+    if (animating) return
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1
+    setActiveIndex(nextIndex)
+  }
 
   const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  };
+    if (animating) return
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1
+    setActiveIndex(nextIndex)
+  }
 
   const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  };
+    if (animating) return
+    setActiveIndex(newIndex)
+  }
 
   const slides = items.map((item) => {
     return (
@@ -62,8 +64,8 @@ function TestimonialCarousel(props) {
           captionHeader={item.caption}
         />
       </CarouselItem>
-    );
-  });
+    )
+  })
 
   return (
     <div>
@@ -74,7 +76,12 @@ function TestimonialCarousel(props) {
               background: #ffcd51;
             }`}
       </style>
-      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+      <Carousel
+        className="carouselMedia"
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+      >
         <CarouselIndicators
           items={items}
           activeIndex={activeIndex}
@@ -93,7 +100,7 @@ function TestimonialCarousel(props) {
         />
       </Carousel>
     </div>
-  );
+  )
 }
 
-export default TestimonialCarousel;
+export default TestimonialCarousel
